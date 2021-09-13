2efcbd8e468b82b8f7f768dea6e2352f9eee5bf5
NileshDebix <90251435+NileshDebix@users.noreply.github.com>
2021-09-13 23:57:51 +0200

    Add files via upload

---
+++ customer.py
@@ -0,0 +1,3 @@
+def format_customer (customer_name, customer_last_name, customer_location):
+    return "Customers name is " + " " + customer_name + " " + customer_last_name + " " + "And lives in" + " " + customer_location
+
